cea5d6813a18f2ef2ead08e7b5f8b65dbb3b6160
Ian Moore <ianmooregis@gmail.com>
2017-02-26 14:01:27 -0600

    Greet new users and alert other users

--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ server.listen(PORT, () => console.log(`Started up on port ${PORT}`));
 io.on('connection', (socket) =>{
     console.log('New user connected');
     
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage: ', message);
         //below sends to all connections and not just one
@@ -24,6 +36,12 @@ io.on('connection', (socket) =>{
             text: message.text,
             createdAt: new Date().getTime()
         });
+        //below send to all connections except the one that sent the message
+        //socket.broadcast.emit('newMessage', {
+        //    from: message.from,
+        //    text: message.text,
+        //    createdAt: new Date().getTime()
+        //});
     });
     
     socket.on('disconnect', () => {
