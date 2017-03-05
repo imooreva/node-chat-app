29d2ea0ed572ca972f0d7cca23eea9b90ca1d0e8
Ian Moore <ianmooregis@gmail.com>
2017-03-04 18:32:53 -0600

    Added autoscroll function with jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom() {
+    //selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child')
+    //heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -17,6 +34,7 @@ socket.on('newMessage', function (message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {    
@@ -29,6 +47,7 @@ socket.on('newLocationMessage', function (message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
