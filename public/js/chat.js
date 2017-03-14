3df0e3eeb6c295740742c0f665832e530e5a543f
Ian Moore <ianmooregis@gmail.com>
2017-03-14 10:39:08 -0500

    Added updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,8 +17,7 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function() {
-    let params = jQuery.deparam(window.location.search);
-    
+    let params = jQuery.deparam(window.location.search);    
     socket.emit('join', params, function(err) {
         if (err) {
             alert(err);
@@ -31,6 +30,14 @@ socket.on('connect', function() {
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function(users) {
+    let ol = jQuery('<ol></ol>');    
+    users.forEach(function(user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+    jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function(message) {
--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message.js');
 const {isRealString} = require('./utils/validation.js')
+const {Users} = require('./utils/users.js');
 const publicPath = path.join(__dirname, '../public'); //helps middleware work properly
 const PORT = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath)); //middleware
 server.listen(PORT, () => console.log(`Started up on port ${PORT}`));
@@ -20,14 +22,13 @@ io.on('connection', (socket) =>{
     
     socket.on('join', (params, callback) => {
         if (!isRealString(params.name) || !isRealString(params.room)) {
-            callback('Name and room name are required.');
+            return callback('Name and room name are required.');
         };
         socket.join(params.room);
-        //socket.leave('The Lounge')
-        //io.emit -> io.to('The Lounge').emit
-        //socket.broadcast.emit -> socket.broadcast.to('The Lounge').emit
-        //socket.emit
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
         
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
         socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));    
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
         callback();
@@ -44,6 +45,11 @@ io.on('connection', (socket) =>{
     });
     
     socket.on('disconnect', () => {
-        console.log('User was disconnected');
+        let user = users.removeUser(socket.id);
+        
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+        }
     });
 });
